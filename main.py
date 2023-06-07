4fd0ce3a337a1a6e45c35075b01b40efd0f1e3d2
NicoliSte <nickstefan4@gmail.com>
2023-06-07 16:23:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,105 +1,102 @@
-from mip import Model, xsum, maximize, BINARY
+import numpy as np
 
+class Node:
+    def __init__(self, level, value, pruned_type, solution):
+        self.level = level  # Nível do nó na árvore de busca
+        self.value = value  # Valor do nó
+        self.pruned_type = pruned_type  # Tipo de poda do nó
+        self.solution = solution  # Solução associada ao nó
 
 def branch_and_bound(c, A, b):
-  # Inicialização
-  best_obj = float('-inf')
-  best_solution = None
-  open_nodes = []
-  all_branches = []  # Lista para armazenar todas as ramificações feitas
+    n = len(c)
+    best_solution = None  # Melhor solução encontrada
+    best_value = float('-inf')  # Melhor valor encontrado
+    active_nodes = []  # Lista de nós ativos
 
-  # Criar o nó raiz
-  root = {
-    'lower_bound': float('-inf'),
-    'upper_bound': float('inf'),
-    'solution': []
-  }
-  open_nodes.append(root)
+    # Cria o nó raiz e o adiciona à lista de nós ativos
+    root_node = Node(0, 0, None, [0] * n)
+    active_nodes.append(root_node)
 
-  while open_nodes:
-    # Selecionar próximo nó a ser resolvido
-    node = open_nodes.pop()
+    while active_nodes:
+        node = active_nodes.pop()
 
-    # Verificar se o nó precisa ser resolvido
-    if node['lower_bound'] > best_obj:
-      continue
+        print_node(node)  # Imprime o nó atual
 
-    # Criar modelo para resolver o subproblema linear
-    model = Model()
+        if node.level == n:
+            # Se o nível do nó for igual ao número de variáveis, chegamos a uma solução completa
+            # Verifica se a solução atual é a melhor encontrada até agora
+            if node.value > best_value:
+                best_solution = node.solution
+                best_value = node.value
+            continue
 
-    # Variáveis binárias
-    x = [model.add_var(var_type=BINARY) for _ in range(len(c))]
+        x = np.array(node.solution)
+        x[node.level] = 1
+        if is_feasible(x, A, b):
+            # Se atribuir 1 à variável atual for factível, cria um novo nó com essa atribuição e o adiciona à lista de nós ativos
+            value = np.dot(c, x)
+            if value > best_value:
+                best_value = value
+                best_solution = x.tolist()
+            active_nodes.append(Node(node.level + 1, value, None, x.tolist()))
+            print_tree_node(node.level + 1, '├──', x.tolist())
 
-    # Função objetivo
-    model.objective = maximize(xsum(c[i] * x[i] for i in range(len(c))))
+        x[node.level] = 0
+        if is_feasible(x, A, b):
+            # Se atribuir 0 à variável atual for factível, cria um novo nó com essa atribuição e o adiciona à lista de nós ativos
+            value = np.dot(c, x)
+            if value > best_value:
+                best_value = value
+                best_solution = x.tolist()
+            # Verifica se o nó pode ser podado
+            if value <= best_value:
+                active_nodes.append(
+                    Node(node.level + 1, value, "Upper Bound", x.tolist()))
+                print_tree_node(node.level + 1, '└──', x.tolist())
 
-    # Restrições
-    for j in range(len(A)):
-      model += xsum(A[j][i] * x[i] for i in range(len(c))) <= b[j]
+    return best_solution, best_value
 
-    # Resolver o subproblema linear
-    model.optimize()
+def is_feasible(x, A, b):
+    # Verifica se a solução x é factível em relação às restrições lineares representadas por A e b
+    return np.all(np.dot(A, x) <= b)
 
-    # Verificar se a solução é inteira
-    if model.num_solutions:
-      obj = model.objective_value
-      solution = [int(x[i].x) for i in range(len(c))]
+def read_input(filename):
+    # Lê o arquivo de entrada e retorna os valores necessários para o problema
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        num_vars, num_constraints = map(int, lines[0].strip().split())
+        c = list(map(int, lines[1].strip().split()))
+        A = []
+        b = []
+        for i in range(2, num_constraints + 2):
+            constraint = list(map(int, lines[i].strip().split()))
+            A.append(constraint[:-1])
+            b.append(constraint[-1])
+        return c, np.array(A), np.array(b)
 
-      # Atualizar a melhor solução encontrada
-      if obj > best_obj:
-        best_obj = obj
-        best_solution = solution
+def print_node(node):
+    # Imprime informações sobre o nó atual
+    print(f"Nível: {node.level}, Valor: {node.value}")
 
-    # Verificar se a solução é fracionária
-    if any(not x[i].x.is_integer() for i in range(len(c))):
-      # Encontrar variável mais fracionária
-      frac_var = max(range(len(c)), key=lambda i: abs(x[i].x - 0.5))
+def print_tree_node(level, connector, value):
+    # Imprime informações sobre um nó na forma de uma árvore
+    indent = '    ' * level
+    fract_value = [f'x[{i}] = {val}' if val % 1 != 0 else f'x[{i}] = {int(val)}' for i, val in enumerate(value)]
+    print(f'{indent}{connector}{", ".join(fract_value)}')
 
-      # Ramificar em dois nós filhos
-      node_1 = {
-        'lower_bound': node['lower_bound'],
-        'upper_bound': node['upper_bound'],
-        'solution': node['solution'] + [(frac_var, 1)]
-      }
-      node_2 = {
-        'lower_bound': node['lower_bound'],
-        'upper_bound': node['upper_bound'],
-        'solution': node['solution'] + [(frac_var, 0)]
-      }
+c, A, b = read_input('teste2.txt')
 
-      # Atualizar limites dos nós filhos
-      node_1['lower_bound'] = max(node_1['lower_bound'], best_obj)
-      node_2['lower_bound'] = max(node_2['lower_bound'], best_obj)
+# Relaxação linear
+relaxed_solution = np.linalg.lstsq(A, b, rcond=None)[0].tolist()
+relaxed_value = np.dot(c, relaxed_solution)
 
-      open_nodes.append(node_1)
-      open_nodes.append(node_2)
+print("Solução Relaxada:")
+for i, val in enumerate(relaxed_solution):
+    print(f'x[{i}] = {val}')
+print("Valor Relaxado:")
+print(relaxed_value)
 
-      # Armazenar as ramificações feitas
-      all_branches.append(node_1)
-      all_branches.append(node_2)
+# Executa o Branch and Bound
+best_solution, best_value = branch_and_bound(c, A, b)
 
-  return best_obj, best_solution, all_branches
-
-
-# Função para ler os dados do arquivo
-def read_input_file(filename):
-  with open(filename, 'r') as file:
-    num_vars, num_constraints = map(int, file.readline().split())
-    c = list(map(int, file.readline().split()))
-    A = []
-    b = []
-    for _ in range(num_constraints):
-      line = list(map(int, file.readline().split()))
-      A.append(line[:-1])
-      b.append(line[-1])
-  return c, A, b
-
-
-# Exemplo de uso
-filename = 'teste4.txt'  # Nome do arquivo de entrada
-c, A, b = read_input_file(filename)
-best_obj, best_solution, all_branches = branch_and_bound(c, A, b)
-
-# Imprimir a solução ótima
-print("Melhor objetivo:", best_obj)
-print("Melhor solução:", best_solution)
+print("\nValor da solução ótima:", best_value)
